0bab0de2521f81b9252ead27ca7b3a7b68408f16
Çağrı Zeki <cagrizeki@gmail.com>
2020-04-21 20:10:47 +0300

    Updates

---
+++ precache-manifest.d366baae876f617553dd9c1b69c1376b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f035d27ac72841690c9664ca9ca315",
+    "url": "/pages1/index.html"
+  },
+  {
+    "revision": "e82ad0a1565a68215fde",
+    "url": "/pages1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "40fbfb30b2baf9c96d3e",
+    "url": "/pages1/static/js/2.73a9e36a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pages1/static/js/2.73a9e36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e82ad0a1565a68215fde",
+    "url": "/pages1/static/js/main.2e6c33e2.chunk.js"
+  },
+  {
+    "revision": "203a72666ad75d3de3b4",
+    "url": "/pages1/static/js/runtime-main.326ad6f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pages1/static/media/logo.5d5d9eef.svg"
+  }
+]);
